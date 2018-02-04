251042cecf3a11bb02865bb504eb199d827a3dca
Jun <wangzhen_tju@126.com>
2018-02-04 15:36:47 +0800

    使用@Around以及ProceedingJoinPoint获得被代理类的信息

--- src/main/java/proxy/BreadProxy.java
+++ src/main/java/proxy/BreadProxy.java
@@ -2,15 +2,15 @@ package proxy;
 
 import java.util.Date;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class BreadProxy {
 
-	@Before("execution(* softcits.spring.demo.Bread.eaten())")
+	/*@Before("execution(* softcits.spring.demo.Bread.eaten())")
 	public void inforBefore(){
 		System.out.println("Bread starts being eaten ----> " + new Date());
 	}
@@ -18,5 +18,19 @@ public class BreadProxy {
 	@After("execution(* softcits.spring.demo.Bread.eaten())")
 	public void inforAfter(){
 		System.out.println("Bread eaten completed  ----> " + new Date());
+	}*/
+	
+	@Around("execution(* softcits.spring.demo.Bread.eaten())")
+	public void inforAround(ProceedingJoinPoint pjp) throws Throwable{
+		System.out.println("Bread starts being eaten ----> " + new Date());
+		//要执行被代理的方法
+		pjp.proceed();
+		
+		System.out.println("被代理的类: "+pjp.getTarget().getClass().getName());
+		
+		System.out.println("被代理的方法: "+pjp.getSignature().getName());
+		
+		System.out.println("Bread eaten completed  ----> " + new Date());
 	}
+	
 }
