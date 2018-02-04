c28f8bcff7787c645655fd2a9b8852943f6b2ae1
Jun <wangzhen_tju@126.com>
2018-02-04 10:01:33 +0800

    Spring的构造器注入

--- src/main/java/inter/Male.java
+++ src/main/java/inter/Male.java
@@ -3,8 +3,8 @@ package inter;
 public class Male implements Person {
 
 	private Bread bread;
-	
-	public void setBread(Bread bread) {
+//没有提供setter方法，而是通过构造器注入	
+	public Male(Bread bread) {
 		this.bread = bread;
 	}
 
