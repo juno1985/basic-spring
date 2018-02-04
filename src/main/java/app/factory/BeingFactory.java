af33d626c05edc27f930a3b18837283424533869
Jun <wangzhen_tju@126.com>
2018-02-04 11:56:55 +0800

    实例工厂方法

--- src/main/java/app/factory/BeingFactory.java
+++ src/main/java/app/factory/BeingFactory.java
@@ -7,7 +7,7 @@ public class BeingFactory
 {
 	// 返回Being实例的静态工厂方法
 	// arg参数决定返回哪个Being类的实例
-	public static Being getBeing(String arg)
+	public  Being getBeing(String arg)
 	{
 		// 调用此静态方法的参数为dog，则返回Dog实例
 		if (arg.equalsIgnoreCase("dog"))
